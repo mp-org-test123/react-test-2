22cd5fa53134b713cfb3047004088cbaf80c504a
shomix <100691266+shomix@users.noreply.github.com>
2022-06-13 01:06:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Milos!
+          Hello Friends! This site is used for testing...
         </p>
         <a
           className="App-link"
